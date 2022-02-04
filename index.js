0c49e17b01f143f975a8dd747ecd69b75e926cda
Zach <zmommaerts@gmail.com>
2022-02-04 16:55:34 -0600

    commit tree 2be1533415dde57ce2498e6fbaf57d961b9b1c85

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 class Polygon {
 
-    constructor([integers]) {
-        this.sides = [integers]
+    constructor(integers) {
+        this.sides = integers
     }
 
     get countSides() {
-        return this.sides.length()
+        return this.sides.length
+    }
+
+    get perimeter() {
+        const reducer = (previousValue, currentValue) => previousValue + currentValue;
+        return this.sides.reduce(reducer)
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+        if (this.sides[0] + this.sides[1] > this.sides[2] && this.sides[1] + this.sides[2] > this.sides[0] && this.sides[0] + this.sides[2] > this.sides[1]) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        if (this.sides[0] === this.sides[1] && this.sides[0] === this.sides[2] && this.sides[0] === this.sides[3]) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        return this.sides[0]**2
     }
 }
